51204f09388882427045a6ac266a29b06f48a359
Brian Miot <brianmiot91@gmail.com>
2025-05-01 12:02:42 -0300

    agregado puerta

--- js/script.js
+++ js/script.js
@@ -38,6 +38,17 @@ new Perfil ("Corrediza", "alto", 6204, "Central", 0.545),
 new Perfil ("Corrediza", "alto", 6205, "Lateral", 0.600)
 ]
 
+const puertaAncho = [
+    new Perfil ("Puerta", "ancho", 6214, "parante", 1.2),
+    new Perfil ("Puerta", "ancho", 6216, "marco", 0.8),
+    new Perfil ("Puerta", "ancho", 6217, "zocalo puerta", 0.536)
+]
+
+const puertaAlto = [
+    new Perfil ("Puerta", "alto", 6215, "parante", 1.2),
+    new Perfil ("Puerta", "alto", 6216, "marco", 0.8)
+]
+
 class Accesorios {
     constructor (tipo, nombre, precio, codigo) {
         this.tipo = tipo,
@@ -49,7 +60,7 @@ class Accesorios {
 }
 felpa = 2000
 
-const accesorios = [ 
+const accesoriosCorrediza = [ 
     new Accesorios ("Corrediza", "R42", 5000, "Rueda Doble"),
     new Accesorios ("Corrediza", "cierre lateral", 2450, "Cierre Lateral"),
     new Accesorios ("Corrediza", "felpon central", 500, "t93"),
@@ -61,23 +72,38 @@ const accesorios = [
     new Accesorios ("Corrediza", "tapa desague camara", 250, "t131")
 ]
 
+const accesoriosPuerta = [
+    new Accesorios ("Puerta", "Escuadra marco", 850, "E69"),
+    new Accesorios ("Puerta", "Escuadra hoja", 500, "E70"),
+    new Accesorios ("Puerta", "Suplemento Escuadra hoja", 500, "E67"),
+    new Accesorios ("Puerta", "Cerradura", 3500, "cerradura"),
+    new Accesorios ("Puerta", "Picaporte", 2500, "H51")
+ ]
+
 
 document.getElementById("cotizacion").onclick = () => {
     const anchoPedido = parseFloat(document.getElementById('ancho').value)
     const altoPedido = parseFloat(document.getElementById('alto').value)
     const totalPerfilAnchoCorrediza = corredizaAncho.reduce((total, perfil) => total + perfil.costoPerfil(), 0) 
     const totalPerfilAltoCorrediza = corredizaAlto.reduce((total, perfil) => total + perfil.costoPerfil(), 0)
-    const sumaAccesorios = accesorios.reduce((total, accesorio) => total + accesorio.precio, 0)
+    const sumaAccesoriosCorrediza = accesoriosCorrediza.reduce((total, accesorio) => total + accesorio.precio, 0)
+    const totalCostoCorrediza =  (anchoPedido * (totalPerfilAnchoCorrediza*2)) + (altoPedido * (totalPerfilAltoCorrediza*2)) + (sumaAccesoriosCorrediza*2)
 
-    const totalCostoCorrediza =  (anchoPedido * (totalPerfilAnchoCorrediza*2)) + (altoPedido * (totalPerfilAltoCorrediza*2)) + (sumaAccesorios*2)
+    const sumaAccesoriosPuerta = accesoriosPuerta.reduce((total, accesorio) => total + accesorio.precio, 0)
+    const totalPerfileriaAnchoPuerta = puertaAncho.reduce((total, perfil) => total + perfil.costoPerfil(), 0) 
+    const totalPerfileriaAltoPuerta = puertaAlto.reduce((total, perfil) => total + perfil.costoPerfil(), 0)
+    const totalCostoPuerta = (anchoPedido * (totalPerfileriaAnchoPuerta*2)) + (altoPedido * (totalPerfileriaAltoPuerta*2)) + (sumaAccesoriosPuerta)
+
     
 
     const resultadoVidrio = (anchoPedido * altoPedido) * vidrio4Mm
-    const resultadoTotal = totalCostoCorrediza + resultadoVidrio
+    const resultadoTotalCorrediza = totalCostoCorrediza + resultadoVidrio
  
 
 document.getElementById("resultado").innerHTML = 
-`Total: $${Math.round(resultadoTotal)}`
+`Total: $${Math.round(resultadoTotalCorrediza)} <br>
+Puerta: $${Math.round(totalCostoPuerta)} <br>
+`
 }
 
 
