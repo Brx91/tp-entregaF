9efc5bd6f6e07c8c15168ae7a25089e07a9f7a45
Brian Miot <brianmiot91@gmail.com>
2025-05-25 17:31:12 -0300

    guardado

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
+
 //Variable precio vidrio
 
-const vidrio4Mm = 6700;
-// id del boton valorVidrio
-// id del parrafo precioVidrio
+const vidrio4Mm = 6700
+const laminado = 32000
+
+//
+
 document.getElementById("valorVidrio").onclick = () => {
-    document.getElementById("precioVidrio").innerHTML = `El precio del vidrio es:  ${vidrio4Mm}`
+    document.getElementById("precioVidrio").innerHTML = `El precio del vidrio de 4mm es:  ${vidrio4Mm}<br> El precio del vidrio laminado 3+3 es: ${laminado}`
 }
 
 const precioAluminio = parseFloat(11240)
+
 
 class Perfil {
     constructor (tipo, seccion, codigo, nombre, pesoMetro) {
@@ -21,6 +25,16 @@ class Perfil {
     costoPerfil() {
     return this.pesoMetro * precioAluminio
     }
+}
+
+class Accesorios {
+    constructor (tipo, nombre, precio, codigo) {
+        this.tipo = tipo,
+        this.nombre = nombre,
+        this.precio = precio,
+        this.codigo = codigo
+    }
+    
 }
 
 const corredizaAncho = [
@@ -38,28 +52,6 @@ new Perfil ("Corrediza", "alto", 6204, "Central", 0.545),
 new Perfil ("Corrediza", "alto", 6205, "Lateral", 0.600)
 ]
 
-const puertaAncho = [
-    new Perfil ("Puerta", "ancho", 6214, "parante", 1.2),
-    new Perfil ("Puerta", "ancho", 6216, "marco", 0.8),
-    new Perfil ("Puerta", "ancho", 6217, "zocalo puerta", 0.536)
-]
-
-const puertaAlto = [
-    new Perfil ("Puerta", "alto", 6215, "parante", 1.2),
-    new Perfil ("Puerta", "alto", 6216, "marco", 0.8)
-]
-
-class Accesorios {
-    constructor (tipo, nombre, precio, codigo) {
-        this.tipo = tipo,
-        this.nombre = nombre,
-        this.precio = precio,
-        this.codigo = codigo
-    }
-    
-}
-felpa = 2000
-
 const accesoriosCorrediza = [ 
     new Accesorios ("Corrediza", "R42", 5000, "Rueda Doble"),
     new Accesorios ("Corrediza", "cierre lateral", 2450, "Cierre Lateral"),
@@ -72,6 +64,17 @@ const accesoriosCorrediza = [
     new Accesorios ("Corrediza", "tapa desague camara", 250, "t131")
 ]
 
+const puertaAncho = [
+    new Perfil ("Puerta", "ancho", 6214, "parante", 1.2),
+    new Perfil ("Puerta", "ancho", 6216, "marco", 0.8),
+    new Perfil ("Puerta", "ancho", 6217, "zocalo puerta", 0.536)
+]
+
+const puertaAlto = [
+    new Perfil ("Puerta", "alto", 6215, "parante", 1.2),
+    new Perfil ("Puerta", "alto", 6216, "marco", 0.8)
+]
+
 const accesoriosPuerta = [
     new Accesorios ("Puerta", "Escuadra marco", 850, "E69"),
     new Accesorios ("Puerta", "Escuadra hoja", 500, "E70"),
@@ -81,31 +84,198 @@ const accesoriosPuerta = [
  ]
 
 
+const ventanaAbrirAncho = [
+    new Perfil ("Ventana Abrir", "ancho", 6216, "marco", 0.8),
+    new Perfil ("Ventana Abrir", "ancho", 6219, "hoja", 0.536)
+]
+
+const ventanaAbrirAlto = [
+    new Perfil ("Ventana Abrir", "alto", 6216, "marco", 0.8),
+    new Perfil ("Ventana Abrir", "alto", 6219, "hoja", 0.536)
+]
+
+const accesoriosVentanaAbrir = [
+    new Accesorios ("Ventana Abrir", "Escuadra marco", 850, "E69"),
+    new Accesorios ("Ventana Abrir", "Falleba", 3500, "H47")
+]
+
+const panioFijoAncho = [
+    new Perfil ("Panio Fijo", "ancho", 6216, "marco", 0.8)
+]
+
+const panioFijoAlto = [
+    new Perfil ("Panio Fijo", "alto", 6216, "marco", 0.8)
+]
+
+const accesoriosPanioFijo = [
+    new Accesorios ("Panio Fijo", "Escuadra marco", 850, "E69")
+]
+
+
 document.getElementById("cotizacion").onclick = () => {
     const anchoPedido = parseFloat(document.getElementById('ancho').value)
     const altoPedido = parseFloat(document.getElementById('alto').value)
+
+    // Calculos Corrediza
     const totalPerfilAnchoCorrediza = corredizaAncho.reduce((total, perfil) => total + perfil.costoPerfil(), 0) 
     const totalPerfilAltoCorrediza = corredizaAlto.reduce((total, perfil) => total + perfil.costoPerfil(), 0)
     const sumaAccesoriosCorrediza = accesoriosCorrediza.reduce((total, accesorio) => total + accesorio.precio, 0)
     const totalCostoCorrediza =  (anchoPedido * (totalPerfilAnchoCorrediza*2)) + (altoPedido * (totalPerfilAltoCorrediza*2)) + (sumaAccesoriosCorrediza*2)
 
+    // Calculos Puerta
     const sumaAccesoriosPuerta = accesoriosPuerta.reduce((total, accesorio) => total + accesorio.precio, 0)
     const totalPerfileriaAnchoPuerta = puertaAncho.reduce((total, perfil) => total + perfil.costoPerfil(), 0) 
     const totalPerfileriaAltoPuerta = puertaAlto.reduce((total, perfil) => total + perfil.costoPerfil(), 0)
-    const totalCostoPuerta = (anchoPedido * (totalPerfileriaAnchoPuerta*2)) + (altoPedido * (totalPerfileriaAltoPuerta*2)) + (sumaAccesoriosPuerta)
+    const totalCostoPuerta = (anchoPedido * (totalPerfileriaAnchoPuerta*2)) + (altoPedido * (totalPerfileriaAltoPuerta*2)) + (sumaAccesoriosPuerta*2)
 
-    
+    //Calculos Ventana de Abrir
+    const sumaAccesoriosVentanaAbrir = accesoriosVentanaAbrir.reduce((total, accesorio) => total + accesorio.precio, 0)
+    const totalPerfileriaAnchoVentanaAbrir = ventanaAbrirAncho.reduce((total, perfil) => total + perfil.costoPerfil(), 0) 
+    const totalPerfileriaAltoVentanaAbrir = ventanaAbrirAlto.reduce((total, perfil) => total + perfil.costoPerfil(), 0)
+    const totalCostoVentanaAbrir = (anchoPedido * (totalPerfileriaAnchoVentanaAbrir*2)) + (altoPedido * (totalPerfileriaAltoVentanaAbrir*2)) + (sumaAccesoriosVentanaAbrir*2)
 
+    //Calculos Paño Fijo
+    const sumaAccesoriosPanioFijo = accesoriosPanioFijo.reduce((total, accesorio) => total + accesorio.precio, 0)
+    const totalPerfileriaAnchoPanioFijo = panioFijoAncho.reduce((total, perfil) => total + perfil.costoPerfil(), 0)
+    const totalPerfileriaAltoPanioFijo = panioFijoAlto.reduce((total, perfil) => total + perfil.costoPerfil(), 0)
+    const totalCostoPanioFijo = (anchoPedido * (totalPerfileriaAnchoPanioFijo*2)) + (altoPedido * (totalPerfileriaAltoPanioFijo*2)) + (sumaAccesoriosPanioFijo*2)
+
+    // Calculos Vidrio
     const resultadoVidrio = (anchoPedido * altoPedido) * vidrio4Mm
+    const resultadoVidrioLaminado = (anchoPedido * altoPedido) * laminado
+    //Resultados
     const resultadoTotalCorrediza = totalCostoCorrediza + resultadoVidrio
- 
+    const resultadoTotalCorredizaLaminado = totalCostoCorrediza + (resultadoVidrioLaminado)
+    const resultadoTotalPuerta = totalCostoPuerta + resultadoVidrio
+    const resultadoTotalPuertaLaminado = totalCostoPuerta + (resultadoVidrioLaminado)
+    const resultadoTotalVentanaAbrir = totalCostoVentanaAbrir + resultadoVidrio
+    const resultadoTotalVentanaAbrirLaminado = totalCostoVentanaAbrir + (resultadoVidrioLaminado)
+    const resultadoTotalPanioFijo = totalCostoPanioFijo + resultadoVidrio
+    const resultadoTotalPanioFijoLaminado = totalCostoPanioFijo + (resultadoVidrioLaminado)
+   
+
+
+let resultadoFinal = ""
+
+
+ switch (document.getElementById("tipoAbertura").value) {
+    case "corrediza":
+        resultadoFinal = resultadoTotalCorrediza
+        if (document.getElementById("tipoVidrio").value == "laminado3_3") {
+            resultadoFinal = resultadoTotalCorredizaLaminado
+        }
+        break
+    case "puerta":
+        resultadoFinal = resultadoTotalPuerta
+        if (document.getElementById("tipoVidrio").value == "laminado3_3") {
+            resultadoFinal = resultadoTotalPuertaLaminado
+        }
+        break
+    case "ventanaAbrir":
+        resultadoFinal = resultadoTotalVentanaAbrir
+        if (document.getElementById("tipoVidrio").value == "laminado3_3") {
+            resultadoFinal = resultadoTotalVentanaAbrirLaminado
+        }
+        break
+    case "paniofijo":
+        resultadoFinal = resultadoTotalPanioFijo
+        if (document.getElementById("tipoVidrio").value == "laminado3_3") {
+            resultadoFinal = resultadoTotalPanioFijoLaminado
+        }
+        break  
+ }
+
+
 
 document.getElementById("resultado").innerHTML = 
-`Total: $${Math.round(resultadoTotalCorrediza)} <br>
-Puerta: $${Math.round(totalCostoPuerta)} <br>
-`
+`Total: $${Math.round(resultadoFinal)}`
+
 }
 
+let listaPedido = []
 
+document.getElementById("agregar").onclick = () => {
+    const ancho = document.getElementById("ancho").value
+    const alto = document.getElementById("alto").value
+    const tipo = document.getElementById("tipoAbertura").value
+    const vidrio = document.getElementById("tipoVidrio").value
+    const total = document.getElementById("resultado").innerText
+
+    const nuevoPedido = {
+        tipo,
+        vidrio,
+        ancho,
+        alto,
+        total,
+        cantidad: 1
+    }
+
+
+   
+const swalWithBootstrapButtons = Swal.mixin({
+  customClass: {
+    confirmButton: "btn btn-success",
+    cancelButton: "btn btn-danger"
+  },
+  buttonsStyling: false
+});
+swalWithBootstrapButtons.fire({
+  title: "Desea agregarlo como pedido?",
+  icon: "warning",
+  showCancelButton: true,
+  confirmButtonText: "Si, Agregarlo",
+  cancelButtonText: "No",
+  reverseButtons: true
+}).then((result) => {
+  if (result.isConfirmed) {
+     listaPedido.push(nuevoPedido)
+    swalWithBootstrapButtons.fire({
+      title: "Producto agregado",
+      text: "Se ha agregado el producto al pedido",
+      icon: "success"
+    });
+  } else if (
+   
+    result.dismiss === Swal.DismissReason.cancel
+  ) {
+    swalWithBootstrapButtons.fire({
+      title: "Se cancelo",
+      text: "No se agrego el producto al pedido",
+      icon: "error"
+    });
+  }
+});
+}
+
+document.getElementById("verPedido").onclick = () => {
+    if (listaPedido.length === 0) {
+        Swal.fire("No hay pedidos todavía.");
+        return;
+    }
+
+    let contenidoHTML = listaPedido.map((pedido, index) => `
+        <strong>Pedido #${index + 1}</strong><br>
+        Abertura: ${pedido.tipo}<br>
+        Vidrio: ${pedido.vidrio}<br>
+        Medidas: ${pedido.ancho} x ${pedido.alto}<br>
+        ${pedido.total}<br><hr>
+        <div style="display: flex; align-items: center; gap: 10px;">
+                <button id="plusButton" onClick="(${pedido.cantidad + 1})">+</button>
+                <span>${pedido.cantidad}</span>
+                <button id="minusButton" onClick="(${pedido.cantidad - 1})">-</button>
+                <button onclick="()">Borrar</button>
+            </div>
+        </div>
+        <hr>
+    `).join("");
+
+     Swal.fire({
+        title: "Lista de pedidos",
+        html: contenidoHTML,
+        showConfirmButton: false,
+        width: 600,
+     })
+       
+    }
 
 
